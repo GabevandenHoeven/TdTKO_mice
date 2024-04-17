0cc302318bde4db973738f5f9a41416716390c12
Gabe van den Hoeven <55189858+GabevandenHoeven@users.noreply.github.com>
2024-04-17 13:40:12 +0200

    Fixed the incidence plots, First filtered all sequences by incidence, then per incidence group calculate the fraction of sequences that match the expression

--- code/abundance_sequences.py
+++ code/abundance_sequences.py
@@ -2,35 +2,57 @@ import csv
 from plots import plot_line_and_scatter_per_incidence
 
 
-def get_abundance(filename: str):
-    """
-    :param filename: str - The name of the file with sequences
+def get_abundance(filename: str, exp: str, max_incidence: int):
+    """This function reads a file with CDR3 sequences of mice annotated with V and J identifiers,
+    (inferred) D sequence, as well as mouse identifier and (inferred) insertions. It stores the incidence of the
+    sequences, then sorts them by incidence, and finally calculates the fraction of sequences per incidence group that
+    match a given expression.
 
-    :return:
+    :param filename: str - The name of the file with sequences
+    :param exp: str - An expression to check for a specific D length. Write in format 'seq[3] == 0'
+    :param max_incidence: int - The number of mice present in the file. If a sequence is present in all mice, this is
+    the maximum incidence.
+    :returns:
+    :return fractions: list - A list with the fractions of sequences that match an expression per incidence.
+    :return incidences: list - A list with lists of sequence information per incidence
     """
     sequences = {
-        # sequence: [count, [d_lengths], [mice], [VJ distances], [insertions]]
+        # V_identifier + sequence J_identifier: [count, sequence, [mice], d_length, VJ distance, insertions]
     }
     with open(filename, 'r') as file:
         reader = csv.reader(file, delimiter='\t')
         header = next(reader)
         for line in reader:
-            mouse, sequence, d_length, v, j, pheno, vj_dis, ins = line[header.index('mouse')], \
+            mouse, sequence, d_length, v, j, vj_dis, ins = line[header.index('mouse')], \
                 line[header.index('Junction.nucleotide.sequence')], int(line[header.index('D.length.used')]), \
-                line[header.index('V.gene')], line[header.index('J.gene')], line[header.index('phenotype')], \
+                line[header.index('V.gene')], line[header.index('J.gene')], \
                 int(line[header.index('V.J.distance')]), int(line[header.index('insertion.length')])
             try:
                 if mouse in sequences[v + sequence + j][2]:
-                    print('This sequence from this mouse is already in the dictionary')
+                    raise ValueError('ValueError\nThis sequence from this mouse is already in the dictionary')
+                elif d_length != sequences[v + sequence + j][3]:
+                    raise ValueError('ValueError\nThe same sequence has a different D length')
+                elif vj_dis != sequences[v + sequence + j][4]:
+                    raise ValueError('ValueError\nThe same sequence has a different VJ distance')
+                elif ins != sequences[v + sequence + j][5]:
+                    raise ValueError('ValueError\nThe same sequence has a different insertion length')
                 else:
                     sequences[v + sequence + j][0] += 1
-                    sequences[v + sequence + j][1].append(d_length)
                     sequences[v + sequence + j][2].append(mouse)
-                    sequences[v + sequence + j][3].append(vj_dis)
-                    sequences[v + sequence + j][4].append(ins)
             except KeyError:
-                sequences.update({v + sequence + j: [1, [d_length], [mouse], [vj_dis], [ins]]})
+                sequences.update({v + sequence + j: [1, sequence, [mouse], d_length, vj_dis, ins]})
 
+    fractions = []
+    incidences = []
+    for i in range(1, max_incidence + 1):
+        # i is the incidence or number of mice the sequence is found in.
+        tmp_list = [c for c in sequences.values() if c[0] == i]
+        # Make a list per group of incidence
+        incidences.append(tmp_list)
 
-    return
+        n_seq_per_incidence = sum(1 for seq in tmp_list if eval(exp))
+        # How many sequences in each incidence group match the expression
+        n_rows = len(tmp_list)
+        fractions.append(n_seq_per_incidence / n_rows * 100)
+    return fractions, incidences
 
--- code/d_per_incidence.py
+++ code/d_per_incidence.py
@@ -12,40 +12,28 @@ if __name__ == '__main__':
     x = []
     y = []
 
-    # for file in files:
-    #     x.append([i for i in range(1, mice_per_file[files.index(file)] + 1)])
-    #     seqs, n_rows = get_abundance(file, 'd_length >= 0')
-    #     mean_d_lengths = []
-    #     for i in range(1, mice_per_file[files.index(file)] + 1):
-    #         n_seq_per_incidence = sum(1 for c in seqs.values() if c[0] == i)
-    #         # i is the incidence or number of mice the sequence is found in.
-    #         # Here you get the number of sequences matching the expression per incidence.
-    #         try:
-    #             mean_d = sum(c[1][0] for c in seqs.values() if c[0] == i) / n_seq_per_incidence
-    #             mean_d_lengths.append(mean_d)
-    #         except ZeroDivisionError:
-    #             mean_d = 0
-    #
-    #     y.append(mean_d_lengths)
-    # plot_line_and_scatter_per_incidence(x, y, ['TdTKO', 'Normal'], ['Incidence', 'Inferred D-segment length (nt)'],
-    #                                     'Mean inferred D-length',
-    #                                     'C:\\Users\\gabev\\PycharmProjects\\MRP_TdTKO_mice\\img'
-    #                                     '\\Mean_inferred_D_length_per_incidence.png')
+    for file in files:
+        max_incidence = mice_per_file[files.index(file)]
+        x.append([i for i in range(1, max_incidence + 1)])
+        fract_incidence, seq_per_incidence = get_abundance(file, 'seq[3] >= 0', max_incidence)
+        mean_d_lengths = []
+        for incidence in range(max_incidence):
+            incidence_group = seq_per_incidence[incidence]
+            mean_d_lengths.append(sum(seq[3] for seq in incidence_group) / len(seq_per_incidence[incidence]))
+        y.append(mean_d_lengths)
 
-    # :param exp: str - An expression to check for a specific D length
-    # 'd_length == 0'
+    plot_line_and_scatter_per_incidence(x, y, ['TdTKO', 'Normal'], ['Incidence', 'Inferred D-segment length (nt)'],
+                                        'Mean inferred D-length',
+                                        'C:\\Users\\gabev\\PycharmProjects\\MRP_TdTKO_mice\\img'
+                                        '\\Mean_inferred_D_length_per_incidence.png')
 
     x = []
     y = []
     for file in files:
-        max_incidence = mice_per_file[files.index(file)] + 1
-        x.append([i for i in range(1, max_incidence)])
-        seqs, n_rows = get_abundance(file, max_incidence)
-        incidences = []
-        for i in range(1, mice_per_file[files.index(file)] + 1):
-            n_seq_per_incidence = sum(1 for c in seqs.values() if c[0] == i)
-            incidences.append(n_seq_per_incidence / n_rows * 100)
-        y.append(incidences)
+        max_incidence = mice_per_file[files.index(file)]
+        x.append([i for i in range(1, max_incidence + 1)])
+        fract_incidences = get_abundance(file, 'seq[3] == 0', max_incidence)
+        y.append(fract_incidences)
     plot_line_and_scatter_per_incidence(x, y, ['TdTKO', 'Normal'], ['Incidence', 'Percentage of sequences (%)'],
                                         'Inferred D-length = 0 nt',
                                         'C:\\Users\\gabev\\PycharmProjects\\MRP_TdTKO_mice\\img'
@@ -53,29 +41,26 @@ if __name__ == '__main__':
     x = []
     y = []
     for file in files:
-        x.append([i for i in range(1, mice_per_file[files.index(file)] + 1)])
-        seqs, n_rows = get_abundance(file, 'd_length <= 2')
-        incidences = []
-        for i in range(1, mice_per_file[files.index(file)] + 1):
-            n_seq_per_incidence = sum(1 for c in seqs.values() if c[0] == i)
-            incidences.append(n_seq_per_incidence / n_rows * 100)
-        y.append(incidences)
+        max_incidence = mice_per_file[files.index(file)]
+        x.append([i for i in range(1, max_incidence + 1)])
+        fract_incidences = get_abundance(file, 'seq[3] <= 2', max_incidence)
+        y.append(fract_incidences)
     plot_line_and_scatter_per_incidence(x, y, ['TdTKO', 'Normal'], ['Incidence', 'Percentage of sequences (%)'],
                                         'Inferred D-length <= 2 nt',
                                         'C:\\Users\\gabev\\PycharmProjects\\MRP_TdTKO_mice\\img'
                                         '\\Fraction_max_2nt_D_per_incidence.png')
 
-    data = []
-    for file in files:
-        seqs, n_rows = get_abundance(file, 'd_length >= 0')
-        d_lengths = []
-        for i in range(1, mice_per_file[files.index(file)] + 1):
-            d_lengths.append([c[1][0] for c in seqs.values() if c[0] == i])
-        data.append(d_lengths)
-    plot_boxplot_per_incidence(data, ['TdTKO', 'Normal'],
-                               [[1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 22, 23], [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]],
-                               ['Incidence', 'D length (nt)'],
-                               [1.5, 3.5, 5.5, 7.5, 9.5, 11.5, 13.5, 15.5, 17.5, 19.5, 21, 22, 23],
-                               list(numpy.arange(1, 14)), 'D length per incidence',
-                               'C:\\Users\\gabev\\PycharmProjects\\MRP_TdTKO_mice\\img'
-                               '\\Boxplot_D_length_per_incidence.png')
+    # data = []
+    # for file in files:
+    #     seqs, n_rows = get_abundance(file, 'd_length >= 0')
+    #     d_lengths = []
+    #     for i in range(1, mice_per_file[files.index(file)] + 1):
+    #         d_lengths.append([c[1][0] for c in seqs.values() if c[0] == i])
+    #     data.append(d_lengths)
+    # plot_boxplot_per_incidence(data, ['TdTKO', 'Normal'],
+    #                            [[1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 22, 23], [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]],
+    #                            ['Incidence', 'D length (nt)'],
+    #                            [1.5, 3.5, 5.5, 7.5, 9.5, 11.5, 13.5, 15.5, 17.5, 19.5, 21, 22, 23],
+    #                            list(numpy.arange(1, 14)), 'D length per incidence',
+    #                            'C:\\Users\\gabev\\PycharmProjects\\MRP_TdTKO_mice\\img'
+    #                            '\\Boxplot_D_length_per_incidence.png')
