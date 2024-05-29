5e72f67375f37e0c38179247c3b9074093adf629
Gabe van den Hoeven <55189858+GabevandenHoeven@users.noreply.github.com>
2024-05-29 16:52:17 +0200

    Still leftover from when I changed the directory structure, the filenames in this file where out-of-date

--- code/test_scripts/check_for_sequences_with_insertions.py
+++ code/test_scripts/check_for_sequences_with_insertions.py
@@ -8,27 +8,33 @@ def check_insertions(filename: str, delim='\t'):
     :return:
     """
     insertion_count = 0
+    line_count = 0
     with open(filename, 'r') as infile:
         reader = csv.reader(infile, delimiter=delim)
         header = next(reader)
         for line in reader:
+            line_count += 1
             l_ins, r_ins, l_palindromic, r_palindromic = int(line[header.index('Left.insertion.length')]), \
                 int(line[header.index('Right.insertion.length')]), int(line[header.index('Left.palindromic')]), \
                 int(line[header.index('Right.palindromic')])
             if (l_ins - l_palindromic) + (r_ins - r_palindromic) != 0:
                 insertion_count += 1
 
-    return insertion_count
+    return insertion_count, line_count
 
 
 if __name__ == '__main__':
     files = [
-        '..\\..\\data_files\\TdTKO\\filtered_data\\filtered_data_exp_TdTKO.tsv',
-        '..\\..\\data_files\\Normal\\filtered_data\\filtered_data_exp_Normal.tsv',
-        '..\\..\\data_files\\TdTKO\\filtered_data\\filtered_data_gen_TdTKO.tsv',
-        '..\\..\\data_files\\Normal\\filtered_data\\filtered_data_gen_Normal.tsv'
+        '..\\..\\data_files\\TdTKO\\filtered_data\\filtered_data_exp_TdTKO_v1.tsv',
+        '..\\..\\data_files\\TdTKO\\filtered_data\\filtered_data_exp_TdTKO_v2.tsv',
+        '..\\..\\data_files\\Normal\\filtered_data\\filtered_data_exp_Normal_v1.tsv',
+        '..\\..\\data_files\\Normal\\filtered_data\\filtered_data_exp_Normal_v2.tsv',
+        '..\\..\\data_files\\TdTKO\\filtered_data\\filtered_data_gen_TdTKO_v1.tsv',
+        '..\\..\\data_files\\TdTKO\\filtered_data\\filtered_data_gen_TdTKO_v2.tsv',
+        '..\\..\\data_files\\Normal\\filtered_data\\filtered_data_gen_Normal_v1.tsv',
+        '..\\..\\data_files\\Normal\\filtered_data\\filtered_data_gen_Normal_v2.tsv'
     ]
     for file in files:
-        count = check_insertions(file)
+        insertions, lines = check_insertions(file)
         fn = file.split('\\')[-1]
-        print(f'Number of duplicate sequences in file \"{fn}\": {count}')
+        print(f'Number of sequences with insertions in file \"{fn}\": {insertions}. {insertions / lines}')
